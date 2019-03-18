dacd0f127669cd01c706c917dd4dae48ca59b9e6
duskhorizon <duskhorizon@oldtrafford.home>
2019-03-18 11:10:08 +0100

    Fixes in magazym models

--- magazyn/models.py
+++ magazyn/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Produkt(models.Model):
+    nazwa = models.CharField(max_length=200)
+    ilosc = models.IntegerField()
+    minimum = models.IntegerField()
+
+class Surowiec(models.Model):
+    nazwa = models.CharField(max_length=200)
+    ilosc = models.DecimalField(decimal_places=2,max_digits=6)
+    minimum = models.DecimalField(decimal_places=2,max_digits=6)
+
