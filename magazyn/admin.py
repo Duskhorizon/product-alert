fde285cee5d6fbe7498479568bff5ff58054e585
duskhorizon <duskhorizon@oldtrafford.home>
2019-03-18 13:01:21 +0100

    Question

--- magazyn/admin.py
+++ magazyn/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Produkt
+from .models import Surowiec
 # Register your models here.
+admin.site.register(Produkt)
+admin.site.register(Surowiec)
